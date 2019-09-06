3dc1b0978db8e98c65ccde984573e6a5360dabe2
Oliver Sun <olisun@mac.com>
2019-09-05 22:15:11 -0700

    created brainstorm md file for psuedo coding and ideas for app. added radio button in html and background image in css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,46 @@
-var hello = 'Hello World'
-var question = 'Whats your name?'
-var goodbye = 'Goodbye'
+var firstGreeting = 'Hello World'
+var secondGreeting = 'How are you doing today?'
+var thirdGreeting = 'What a beautiful day it is!'
 var counter = 10;
+var gameOver = 'GAME OVER';
 
-setTimeout(function firstGreeting() {
-  $('#test1').append(hello)
-}, 1000 * 1)
+// Function to append 3 strings to the DOM in sequential order
 
-setTimeout(function firstGreeting() {
-  $('#test1').empty();
-}, 1000 * 2)
-
-setTimeout(function secondGreeting() {
-  $('#test1').append(question)
-}, 1000 * 3)
-
-setTimeout(function firstGreeting() {
-  $('#test1').empty();
-}, 1000 * 4)
-
-setTimeout(function thirdGreeting() {
-  $('#test1').append(goodbye)
-}, 1000 * 5)
-
-
-// function greeting() {
-//   var wiggly = setInterval(function() {
-//     $('#test1').text(counter)
-//     counter--
-//     if (counter === 0) {
-//       clearInterval(wiggly)
-//     }
+// function helloWord() {
+//   setTimeout(function() {
+//     $('#test1').append(firstGreeting)
 //   }, 1000 * 1)
+
+//   setTimeout(function() {
+//     $('#test1').text('');
+//   }, 1000 * 2)
+
+//   setTimeout(function() {
+//     $('#test1').append(secondGreeting)
+//   }, 1000 * 3)
+
+//   setTimeout(function() {
+//     $('#test1').text('');
+//   }, 1000 * 4)
+
+//   setTimeout(function() {
+//     $('#test1').append(thirdGreeting)
+//   }, 1000 * 5)
 // }
 
-// greeting()
+// helloWord();
+
+// Count down timer
+
+function countDown() {
+  var wiggly = setInterval(function() {
+    $('#test2').text(counter)
+    counter--
+    if (counter === 0) {
+      clearInterval(wiggly)
+      $('#test2').text(gameOver)
+    }
+  }, 1000 * 1)
+}
+
+countDown();
---
+++ brainstorm.md
@@ -0,0 +1,32 @@
+# This file is for planning the app build and pseudo coding.
+
+## Things Needed:
+  1. Start button to start the game
+  2. Count down timer for the game.
+     a. Show time remaining to the player. 
+  3. Radio buttons for HTML multiple choice.
+     a. if/else rules for quiz questions.
+        if correct score:
+          * correct answers++
+
+        else
+          * incorrect answers++
+          * 
+
+  4. Done Button if the player finishes before score runs out.
+  5. function to stop game when the timer hits 0
+  6. Correct Answers sore.
+  7. Incorrect Answers score.
+  8. Unanswered Questions score.
+  9. Global Variables:
+     1.  var counter;
+     2.  var correctAnswers;
+     3.  var incorrectAnswers;
+     4.  
+
+## Topics to review:
+  1. Bootstrap
+  2. setTimeOut
+  3. setInterval
+  4. clearTimeOut
+  5. clearInterval
