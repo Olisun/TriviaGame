2093b44af7c57a2f36fff7ab00412f9075469414
Oliver Sun <olisun@mac.com>
2019-09-06 21:51:13 -0700

    testing different button options html. testing different code options for buttons js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,55 @@
 $(document).ready(function() {
 
   var counter = 0;
-  var correctAnswers = 0;
-  var incorectAnswers = 0;
+  var correctAnswers;
+  var incorectAnswers;
   var isGameOver = false;
+  var nebulaBtn = $('#nebula-btn')
 
   function startQuiz() {
-    $('#asgard-btn').on('click', function(event) {
-      correctAnswers++;
-      $('#correct-answers').text(correctAnswers);
-      $(this).off(event);
 
+    $('#asgard-btn').click(function(event) {
+      if (this.id === 'asgard-btn') {
+        correctAnswers = 0;
+        correctAnswers++;
+        $('#correct-answers').text(correctAnswers);
+        $(this).css("background-color", "red");
+        $(this).css("height", "15px");
+        $(this).off(event);
+        nebulaBtn.css("background-color", "none");
+      }
     });
 
-
+    $('#nebula-btn').click(function(event) {
+      if (this.id === 'nebula-btn') {
+        incorrectAnswers = 0;
+        incorrectAnswers++;
+        $('#incorrect-answers').text(incorrectAnswers);
+        $(this).css("background-color", "red");
+        $(this).css("height", "15px");
+        $(this).off(event);
+      }
+    });
   }
 
+  // function incorrect() {
+
+  //   $('#asgard-btn').on('click', function(event) {
+  //     correctAnswers++;
+  //     $('#correct-answers').text(correctAnswers);
+  //     $(this).css("background-color", "red");
+  //     $(this).css("height", "15px");
+  //     $(this).off(event);
+  //   });
+
+  //   $('#rolling-stones-btn').on('click', function(event) {
+  //     correctAnswers++;
+  //     $('#correct-answers').text(correctAnswers);
+  //     $(this).off(event);
+  //     $(this).css("background-color", "red");
+  //     $(this).css("height", "15px");
+  //   });
+  // }
   // function keepScore() {
 
   // }
