3035b8876cd29af85b7596549c0a52973d03fdc0
Oliver Sun <olisun@mac.com>
2019-09-07 18:29:01 -0700

    added unanswered questions field in html. refactored score-board styling and code in css and js

--- assets/javascript/appTest.js
+++ assets/javascript/appTest.js
@@ -3,13 +3,12 @@ $(document).ready(function() {
   $('#done').on('click', done);
 
   var wiggly;
-  var timeRemaining = 132;
+  var timeRemaining = 12;
   var correctAnswers = 0;
   var incorrectAnswers = 0;
+  var unAnswered = 0;
   var gameOver = 'GAME OVER'
   var isGameOver = false;
-
-
 
   function timeConverter(t) {
     var minutes = Math.floor(t / 60);
@@ -27,30 +26,41 @@ $(document).ready(function() {
 
   function start() {
     $('.radio-buttons').css("display", "inline");
+    $('.score-board').css("display", "none");
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    timeRemaining = 12;
+    startQuiz();
     wiggly = setInterval(function() {
       var converted = timeConverter(timeRemaining);
       $('#countdown-clock').text(converted)
       timeRemaining--;
       if (timeRemaining === 0) {
         clearInterval(wiggly)
-        timeRemaining = 132;
+        done();
+        timeRemaining = 12;
         $('#countdown-clock').text(gameOver);
       }
     }, 1000 * 1)
   }
 
   function done() {
+    $('.radio-buttons').css("display", "none");
     $('.score-board').css("display", "inline");
+    $('#incorrect-answers').text(incorrectAnswers);
+    $('#correct-answers').text(correctAnswers);
+
     clearInterval(wiggly);
   }
 
   function startQuiz() {
+    $('#incorrect-answers').text(incorrectAnswers);
+    $('#correct-answers').text(correctAnswers);
 
     $("input[value='earth']").click(function() {
       var radioValue = $("input[value='earth']:checked").val();
       if (radioValue) {
         incorrectAnswers++;
-        $('#incorrect-answers').text(incorrectAnswers);
         $("input[value='earth']").off()
       }
     });
@@ -59,7 +69,6 @@ $(document).ready(function() {
       var radioValue = $("input[value='asgard']:checked").val();
       if (radioValue) {
         correctAnswers++;
-        $('#correct-answers').text(correctAnswers);
         $("input[value='asgard']").off()
       }
     });
@@ -68,7 +77,6 @@ $(document).ready(function() {
       var radioValue = $("input[value='five']:checked").val();
       if (radioValue) {
         incorrectAnswers++;
-        $('#incorrect-answers').text(incorrectAnswers);
         $("input[value='five']").off()
       }
     });
@@ -77,7 +85,6 @@ $(document).ready(function() {
       var radioValue = $("input[value='six']:checked").val();
       if (radioValue) {
         correctAnswers++;
-        $('#correct-answers').text(correctAnswers);
         $("input[value='six']").off()
       }
     });
@@ -86,7 +93,7 @@ $(document).ready(function() {
 
   }
 
-  startQuiz();
+
 
 
 
